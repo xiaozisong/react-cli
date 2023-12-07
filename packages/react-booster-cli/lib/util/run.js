ce8f09a4b53c8595be1b5bf52cfd7509d1b371f3
1170957523@qq.com <1170957523@qq.com>
2023-12-07 23:12:49 +0800

    feat: 创建run函数

---
+++ packages/react-booster-cli/lib/util/run.js
@@ -0,0 +1,7 @@
+const execa = require('execa');
+module.exports = function run (command, args) {
+  if (!args) {
+    [command, ...args] = command.split(/\s+/)
+    return execa(command, args)
+  }
+}
